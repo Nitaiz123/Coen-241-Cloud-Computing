ab92046e6263c0502a062d90386ce5a28c1c2dc8
Nityanand Pujari <67378027+Nitaiz123@users.noreply.github.com>
2024-02-01 15:44:39 -0800

    Add files via upload

---
+++ Homework /Homework 1/docker_script.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#
+
+CPU_SCRIPT_PATH="C:/Program Files/Desktop/cpu_test.sh"
+FILEIO_SCRIPT_PATH="C:/Program Files/Desktop/file_io_test.sh"
+Memory_SCRIPT_PATH="C:/Program Files/Desktop/memory_test.sh"
+
+"$CPU_SCRIPT_PATH"
+"$FILEIO_SCRIPT_PATH"
+"$Memory_SCRIPT_PATH"
